d6a4d33c6421ad61735dffe6cdff4b9c971553ad
hemuinakollu <hemuinakollu@missionrnd.com>
2016-04-21 19:39:55 +0530

    Submit Commit

--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -1,41 +1,8 @@
-/*
-Write a program to generate elements of a matrix in spiral order.
-Store the generated elements in single dimensional array and return that array.
-
-Directions for Spiral are : Go Right ,Go Down ,Go Left, Go Up
-Ex:
-
-Matrix:
-1	2	3
-4	5	6
-7	8	9
-
-Spiral order:
-1	2	3	6	9	8	7	4	5
-
-Given matrix is a two dimensional array(input_array) with dimensions rows, columns.
-If rows, columns are invalid then return NULL.
-If input_array is NULL then return NULL.
-
-Example Input :
-int board[2][3]={
-{4,6,7},
-{1,3,8}
-};
-spiral(2,2,(int **)board); // Rows followed by columns followed by board
-
-Example Output : Return an array consisting of {4,6,7,8,3,1};
-
-Note : Check the function Parameters ,Its a double pointer .
-
-*/
-
-#include "stdafx.h"
 #include<stdlib.h>
 
 void sp_helpero(int rows, int columns, int **input_array, int *arr, int *i, int r, int c)
 {
-	if (r > (rows / 2))
+	if (r > rows / 2)
 		return;
 	else
 	{
@@ -104,9 +71,9 @@ int *spiral(int rows, int columns, int **input_array)
 {
 	if (rows <= 0 || columns <= 0 || input_array == NULL)
 		return NULL;
-	/*else
+	else
 	{
-		int *arr = (int*)malloc(rows*columns);
+		int *arr = (int*)malloc(1000);
 		int i = 0;
 		if (rows % 2 != 0)
 			sp_helpero(rows, columns, input_array, arr, &i, 0, 0);
@@ -115,10 +82,6 @@ int *spiral(int rows, int columns, int **input_array)
 		return arr;
 
 
-	}*/
+	}
 
 }
-
-
-
-
